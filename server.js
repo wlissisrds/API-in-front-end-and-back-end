106a0d231e9437b42e94f9b5aac47663b273024b
wlissis <wlissisrds@outlook.com>
2020-10-19 16:20:28 -0300

    final commit

--- server.js
+++ server.js
@@ -10,12 +10,19 @@ app.use(cors());
 //criando rota de serviço api
 app.get('/', async(req, res) => {
 
-    //response é a resposta do axiosm MAS eu tiro o data de dentro o response
-    const {data} = await axios('https://jsonplaceholder.typicode.com/users')
-    console.log(data);
+    try{
+        //response é a resposta do axiosm MAS eu tiro o data de dentro o response
+        const {data} = await axios('https://jsonplaceholder.typicode.com/users')
+
+        return res.json(data);
+        
+    }catch(error) {
+        console.error(error);
+    }
+
     
 
-    return res.json(data);
+    
 });
 //criando porta de serviço do projeto
 app.listen('4567');
