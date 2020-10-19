100c4b005736bfe31fe9ae0f75b919431a8f9d47
wlissis <wlissisrds@outlook.com>
2020-10-19 16:09:56 -0300

    consumindo api com Axios

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 ## API
  - iniciar projeto `npm init -y`
  - instalar as dependencias `npm i express nodemon cors axios`
+
+ ## Front-end
+ - utilizar o fetch para consumir API backend
+ - CORS serve para habilitar a comunição entre servidores pra consumir API  
+
+ ## Back-end
+ - jasonplaceholder (https://jsonplaceholder.typicode.com/users)
+ - usar AXIOS
+ - semelhanças entre AXIOS e Fetch
+ - usar Try/Catch
--- server.js
+++ server.js
@@ -1,11 +1,21 @@
+const cors = require('cors');
 const express = require('express');
+const axios = require('axios');
+
 
 const app = express();
 
-//criando rota de serviço api
-app.get('/', (req, res) => {
-    return res.json({ menssage : 'okay' })
-});
+app.use(cors());
 
+//criando rota de serviço api
+app.get('/', async(req, res) => {
+
+    //response é a resposta do axiosm MAS eu tiro o data de dentro o response
+    const {data} = await axios('https://jsonplaceholder.typicode.com/users')
+    console.log(data);
+    
+
+    return res.json(data);
+});
 //criando porta de serviço do projeto
-app.listen('3000');
+app.listen('4567');
